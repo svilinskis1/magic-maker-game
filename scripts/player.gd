a92681faf45685af97f6dbf08e661eb391f2c744
evilinskis <eav1582@rit.edu>
2024-02-22 14:29:27 -0500

    made scripts for player and enemies

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,25 +1,37 @@
 extends CharacterBody3D
 
+@onready var ray_cast_3d = $RayCast3D
 
 const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+const MOUSE_SENS = 0.5
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+var can_shoot = true
+var dead = false
 
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _input(event):
+	if dead:
+		return
+	if event is InputEventMouseMotion:
+		rotation_degrees.y -= event.relative.x * MOUSE_SENS
+
+func _process(delta):
+	if Input.is_action_just_pressed("exit"):
+		get_tree().quit()
+	if Input.is_action_just_pressed("restart"):
+		restart()
+	
+	if dead:
+		return
+	if Input.is_action_just_pressed("shoot"):
+		shoot()
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y -= gravity * delta
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("left", "right", "up", "down")
+	if dead:
+		return
+	var input_dir = Input.get_vector("left", "right", "forward", "back")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
@@ -27,5 +39,25 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+	
 	move_and_slide()
+
+
+func restart():
+	get_tree().reload_current_scene()
+
+func shoot():
+	if !can_shoot:
+		return
+	can_shoot = false
+	if ray_cast_3d.is_colliding() and ray_cast_3d.get_collider().has_method("kill"):
+		ray_cast_3d.get_collider().kill()
+		
+func shoot_anim_done():
+	can_shoot = true
+
+func kill():
+	dead = true
+	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	
+
